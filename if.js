2fd51b1b238c55f60520dba987b9ebfed424cae7
PortableTomb <jade.getz@gmail.com>
2016-08-11 10:55:15 -0700

    Update if.js

--- if.js
+++ if.js
@@ -11,3 +11,15 @@ if (1+1 === 2) {
 if (1 + 1 !== 2) {
   console.log('Math is broken.');
 }
+
+if(2>1){
+    console.log("A");
+} else {
+    console.log("B");
+}
+
+if (2 > 1 && 5 <= 3) {
+    console.log('C');
+} else {
+    console.log('D');
+}
