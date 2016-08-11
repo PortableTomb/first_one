613095460ea0a4d7c63099fb8f8d6499d8d213fe
PortableTomb <jade.getz@gmail.com>
2016-08-11 11:48:06 -0700

    Add function.js

---
+++ function.js
@@ -0,0 +1,15 @@
+function greet()  {
+    console.log("Hello, World");
+}
+greet();
+
+function yell() {
+    for (var i = 0; i < 11; i++) {
+      if (i <= 9){
+        console.log("yell");
+    } else {
+        console.log("YELL!");
+    }
+}
+}
+yell();
