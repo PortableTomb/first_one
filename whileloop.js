7623e3094626ae463de427bd75db7edb3ea74cc4
PortableTomb <jade.getz@gmail.com>
2016-08-12 22:19:39 -0700

    Adding JS files

---
+++ whileloop.js
@@ -0,0 +1,22 @@
+var i = 1;
+while (i < 10){
+    console.log(i * i);
+    i++;
+}
+
+var i = 11;
+do {
+    console.log(i * i);
+    i++;
+} while(i < 10);
+
+
+for (var i = 1; i < 11; i++) {
+    console.log(i, 11-i);
+}
+
+var i = 1;
+do {
+    console.log(i, 11-i);
+    i++;
+} while(i < 11);
